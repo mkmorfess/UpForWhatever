c04774a534b1cf184f117e85406bc6c1a3e4cc4a
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-10-31 06:54:54 -0500

    Added Google Maps

---
+++ project1.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+				
+
+		$("#bars").on("click", function() {
+
+			// .html() the top 5 bars in the houston area
+
+		})
+
+		$("#nightlife").on("click", function() {
+
+			// .html() the top 5 clubs in the houston area
+
+		})
+
+		$("#restaurants").on("click", function() {
+
+			// .html() the top 5 restaurants in the houston area
+			
+		})
+
+
+
+
+
+});
