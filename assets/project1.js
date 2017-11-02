826ccbb599ac73201534655c02e92b61428eeb6f
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-11-02 06:41:04 -0500

    Random Restaurant Button Working - Generates 5 restaurants in console.log

--- assets/project1.js
+++ assets/project1.js
@@ -25,18 +25,28 @@ $(document).ready(function(){
 	// API Website https://developers.zomato.com/documentation#!/restaurant/search
 
 var queryURL1 = "https://developers.zomato.com/api/v2.1/cuisines?city_id=277&lat=29.7604&lon=-95.3698"
-var queryURL2 = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=houston&count=5&lat=29.7604&lon=-95.3698&radius=5000&cuisines=Jamaican&sort=real_distance"
 var api_key = "c401c9eff451fccdbc6ac1dbacaa0e55"
-				
+var cuisineList = [];
+	
 	$.ajax({
 		url: queryURL1,
 		headers: { 'user-key': api_key },
 		method: "GET"
 	}).done(function(response){
 
-		console.log(response);
+		for (var i = 0; i < response.cuisines.length; i++) {
+		cuisineList.push(response.cuisines[i].cuisine.cuisine_name);
+	}
 
-	})
+		console.log(cuisineList);
+
+
+ 	$("#random").on("click", function() {
+
+		var cuisineRandom = cuisineList[Math.floor(Math.random() * cuisineList.length)];
+		console.log(cuisineRandom)
+		var queryURL2 = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + cuisineRandom + "&count=5&lat=29.7604&lon=-95.3698&radius=5000&cuisines=" + cuisineRandom + "&sort=real_distance"
+
 
 	$.ajax({
 		url: queryURL2,
@@ -45,34 +55,43 @@ var api_key = "c401c9eff451fccdbc6ac1dbacaa0e55"
 	}).done(function(response) {
 
 		console.log(response);
-	})
+
+		for (var i = 0; i < response.restaurants.length; i++) {
+
+		console.log("Restaurant Name: " + response.restaurants[i].restaurant.name);
+		console.log("Average Cost For Two: " + response.restaurants[i].restaurant.average_cost_for_two);
+		console.log("Image: " + response.restaurants[i].restaurant.photos_url);
+		console.log("Rating: " + response.restaurants[i].restaurant.user_rating.aggregate_rating);
+		console.log("Rank: " + response.restaurants[i].restaurant.user_rating.rating_text);
+		console.log("Number Of Votes: " + response.restaurants[i].restaurant.user_rating.votes);
+		}
+
+		});
+
+	});
+
+});
 
 
-
-		// This function initiates the map and pastes it to #Map in the HTML
-			
         	
 
+		// $("#bars").on("click", function() {
 
-        	
+		// 	// .html() the top 5 bars in the houston area
 
-		$("#bars").on("click", function() {
+		// })
 
-			// .html() the top 5 bars in the houston area
+		// $("#nightlife").on("click", function() {
 
-		})
+		// 	// .html() the top 5 clubs in the houston area
 
-		$("#nightlife").on("click", function() {
+		// })
 
-			// .html() the top 5 clubs in the houston area
+		// $("#restaurants").on("click", function() {
 
-		})
-
-		$("#restaurants").on("click", function() {
-
-			// .html() the top 5 restaurants in the houston area
+		// 	// .html() the top 5 restaurants in the houston area
 			
-		})
+		// })
 
 
 
