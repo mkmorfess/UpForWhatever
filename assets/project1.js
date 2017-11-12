94aa55c098b0a91baffc5b3df53b8c2962b92377
Michael Morfessis <mkmorfess@yahoo.com>
2017-11-11 19:20:44 -0600

    Added feature to click on table header and sort alphabetically

--- assets/project1.js
+++ assets/project1.js
@@ -355,8 +355,9 @@ function initMap() {
 								}
 
 								$("#moreInfo2").click(function(){
-							            google.maps.event.trigger(markers[0], 'click');
-							        });	
+									window.scrollTo(0, 300);
+							        google.maps.event.trigger(markers[0], 'click');
+							    });	
 
 								dataOne.push(cuisineRandom, response.restaurants[0].restaurant.name, response.restaurants[0].restaurant.user_rating.rating_text, "<a target='_blank' href='" + response.restaurants[0].restaurant.url + "'>More Info</a>");
 							}
@@ -455,10 +456,12 @@ function initMap() {
 								// }
 
 								 $("#moreInfo1").click(function(){
+								 		window.scrollTo(0, 300);
 							            google.maps.event.trigger(markers[0], 'click');
 							        });			
 
 								 $("#moreInfo3").click(function(){
+								 		window.scrollTo(0, 300);
 							            google.maps.event.trigger(markers[1], 'click');
 							        });			
 								dataZero.push(cuisineRandom, response.restaurants[0].restaurant.name, response.restaurants[0].restaurant.user_rating.rating_text, "<a target='_blank' href='" + response.restaurants[0].restaurant.url + "'>More Info</a>");
@@ -519,14 +522,17 @@ function initMap() {
 									// console.log(dataTwo);
 
 									$("#moreInfo1").click(function(){
+										window.scrollTo(0, 300);
 							            google.maps.event.trigger(markers[0], 'click');
 							        });
 
 							        $("#moreInfo2").click(function(){
+							        	window.scrollTo(0, 300);
 							            google.maps.event.trigger(markers[1], 'click');
 							        });
 
 							        $("#moreInfo3").click(function(){
+							        	window.scrollTo(0, 300);
 							            google.maps.event.trigger(markers[2], 'click');
 							        });															
 							}				
@@ -539,7 +545,7 @@ function initMap() {
 						userLong = [];
 
 					});
-				window.scrollTo(0, 500);
+				window.scrollTo(0, 650);
 				//document.body.scrollHeight
 				loading = false;
 				}				
@@ -679,8 +685,61 @@ function userRemove () {
 
 userRemove();
 
-	
-
+// this code was found online at w3schools to be able to sort the table for the favorite list
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("main");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc"; 
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
 
 
 
